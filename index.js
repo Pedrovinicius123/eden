76f4f1ba81b999cc6314c1ff0d8ba6652efb5939
Pedrovinicius123 <pvasx123@gmail.com>
2025-02-14 10:07:51 -0300

    Adding express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const http = require('http')
+const express = require('express');
+const app = express();
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send("Jesus é Rei!")
+
+})
+
+app.get('/projeto', (req, res) => {
+    res.json({site: "Projeto Eden"})
+
+})
+
+app.listen(port || 3000, () => {console.log("Servidor rodando")})
